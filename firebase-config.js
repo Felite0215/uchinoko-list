e5daad9a8fa7663dff811046c9a440b2dad8db96
Felite0215 <ouka173@gmail.com>
2025-07-15 23:34:07 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,4 @@
+// あなたの Firebase 設定情報
 const firebaseConfig = {
   apiKey: "AIzaSyADP4_uKX2WsIUFD1qmmnE4JqIma3C-Ceo",
   authDomain: "uchinoko-list.firebaseapp.com",
@@ -6,6 +7,5 @@ const firebaseConfig = {
   messagingSenderId: "631496718874",
   appId: "1:631496718874:web:ed3b8f69b1fc5391996fa3"
 };
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
 
+firebase.initializeApp(firebaseConfig);
